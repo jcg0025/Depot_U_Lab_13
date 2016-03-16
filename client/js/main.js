1925d6ef5e9c62f0946be3761160c3d10b9f1723
Jacob Gossett <jgossett121@gmail.com>
2016-03-16 13:22:00 -0500

    need to reverse tweets

---
+++ client/js/main.js
@@ -0,0 +1,64 @@
+var app = angular.module('myApp', ['ngRoute', 'ngResource'])
+	app.controller('welcomeController', ['$scope', '$location', '$rootScope', function($scope, $location, $rootScope){
+        $scope.welcomeMessage = 'Enter User Name';
+        $rootScope.welcome = {
+            userName: ''
+        }
+        $scope.go = function() {
+            $location.path('tweets/')
+        }
+	}]);
+    app.controller('tweetsController', ['$scope', '$rootScope', '$http', '$resource', function($scope, $rootScope, $http, $resource){
+        $scope.tweetsMessage = 'much tweets';
+        // var tweets = $resource('http://localhost:3000/server/data.json');
+        // $scope.loadTweets = function(){
+        //     tweets.get();
+        // }
+        $http({
+            url: '/messages',
+            method: 'GET'
+        }).then(function(success){
+            $scope.content = success.data;
+        }, function(error){
+            console.log(error);
+        });
+        
+        $scope.welcome = $rootScope.welcome;
+        
+        $scope.tweet = {
+            user: $scope.welcome.userName,
+            text: ''
+        }
+        
+        $scope.postTweet = function() {
+            $http({
+                url: '/messages',
+                method: 'POST',
+                data: $scope.tweet
+            }).then(function(success){
+                $scope.createdAt = new Date();
+                $scope.newContent = success.data;
+                console.log(success);
+            }, function(error){
+                console.log(error);
+            })
+        }
+            
+        
+        
+    }]);
+	app.config(['$routeProvider', function($routeProvider){
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/welcome.html',
+            controller:  'welcomeController'
+        })
+        .when('/tweets', {
+            templateUrl: 'views/tweets.html',
+            controller: 'tweetsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+}]);
